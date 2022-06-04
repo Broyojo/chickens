a0d3b11c69ff6073bb65f69dbcd800dc718572e8
Broyojo <dha@xoba.com>
2022-06-04 15:57:45 -0400

    added automatic run of multiple chicken numbers

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,55 @@
 use radix_fmt::radix;
 use rayon::prelude::*;
 
-const NUM_CHICKENS: u32 = 10;
-
 fn main() {
-    let total = (NUM_CHICKENS as u64).pow(NUM_CHICKENS);
+    for num_chickens in 2..=10 {
+        let total = (num_chickens as u64).pow(num_chickens as u32);
+        let successes = (0..total)
+            .into_par_iter()
+            .map(|n: u64| -> u64 {
+                let s = pad_zeros_left(radix(n, num_chickens).to_string(), num_chickens as usize);
 
-    println!("total: {}", total);
+                //println!("{}", s);
 
-    let successes = (0..total).into_par_iter().map(check).sum::<u64>();
+                let mut counts = vec![0; num_chickens as usize];
 
-    println!("successes: {}, total: {}", successes, total);
-    println!("prob: {}", successes as f64 / total as f64);
+                for c in s.chars() {
+                    counts[c.to_digit(num_chickens as u32).unwrap() as usize] += 1;
+                }
+
+                let mut alive = 0;
+
+                for count in counts {
+                    if count == 0 {
+                        alive += 1;
+                    }
+                }
+
+                //println!("{} {:?}", s, counts);
+
+                if alive == 1 {
+                    1
+                } else {
+                    0
+                }
+            })
+            .sum::<u64>();
+
+        let n_gcd = gcd(successes, total);
+
+        println!(
+            "test with {} chickens: {} successes out of {} total runs: prob = {}/{} ≈ {} ",
+            num_chickens,
+            successes,
+            total,
+            successes / n_gcd,
+            total / n_gcd,
+            successes as f64 / total as f64
+        );
+    }
 }
 
-fn check(n: u64) -> u64 {
-    let s = pad_space_left(
-        radix(n, NUM_CHICKENS as u8).to_string(),
-        NUM_CHICKENS as usize,
-    );
-
-    //println!("{}", s);
-
-    let mut counts = [0; NUM_CHICKENS as usize];
-
-    for c in s.chars() {
-        counts[c.to_digit(NUM_CHICKENS).unwrap() as usize] += 1;
-    }
-
-    let mut alive = 0;
-
-    for count in counts {
-        if count == 0 {
-            alive += 1;
-        }
-    }
-
-    //println!("{} {:?}", s, counts);
-
-    if alive == 1 {
-        1
-    } else {
-        0
-    }
-}
-
-fn pad_space_left(s: String, n: usize) -> String {
+fn pad_zeros_left(s: String, n: usize) -> String {
     if s.len() > n {
         return s;
     }
@@ -61,4 +65,15 @@ fn pad_space_left(s: String, n: usize) -> String {
     padded.push_str(&s);
 
     padded
+}
+
+fn gcd(mut a: u64, mut b: u64) -> u64 {
+    while b != 0 {
+        a %= b;
+        if a == 0 {
+            return b;
+        }
+        b %= a;
+    }
+    a
 }
