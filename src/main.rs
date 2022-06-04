6b0e1d0426fd0632d206b3a4c1e605da1725ad26
Broyojo <dha@xoba.com>
2022-06-03 22:35:19 -0400

    results matching simulatio

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ use rayon::prelude::*;
 
 fn main() {
     let total = 10u64.pow(10) - 1;
-    let offset = 10u64.pow(9);
 
-    let successes = (offset..=total).into_par_iter().map(check).sum::<u64>();
+    let successes = (0..=total).into_par_iter().map(check).sum::<u64>();
 
     println!("successes: {}, total: {}", successes, total);
     println!("prob: {}", successes as f64 / total as f64);
@@ -13,15 +12,22 @@ fn main() {
 fn check(n: u64) -> u64 {
     let s = format!("{:0>10}", n);
 
-    let mut alive = 0;
+    let mut counts = [0; 10];
 
     for c in s.chars() {
-        if c.to_digit(10).unwrap() == 0 {
+        counts[c.to_digit(10).unwrap() as usize] += 1;
+    }
+
+    let mut alive = 0;
+
+    for count in counts {
+        if count == 0 {
             alive += 1;
         }
     }
 
     if alive == 1 {
+        //println!("{} {:?}", s, counts);
         1
     } else {
         0
